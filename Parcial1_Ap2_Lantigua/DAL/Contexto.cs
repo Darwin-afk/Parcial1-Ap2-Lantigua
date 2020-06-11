f8b4e62ca10c038e4c0036ccb5351508ee1e1e81
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-06-10 20:38:20 -0400

    Creacion del Contexto

---
+++ Parcial1_Ap2_Lantigua/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Parcial1_Ap2_Lantigua.Models;
+
+namespace Parcial1_Ap2_Lantigua.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Articulos.db");
+        }
+    }
+}
