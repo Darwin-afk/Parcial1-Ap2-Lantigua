a90effbd84610b65c773fd843e42f69e65fc1837
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-06-10 20:57:12 -0400

    Creacion de la BLL de Articulos

---
+++ Parcial1_Ap2_Lantigua/BLL/ArticulosBLL.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap2_Lantigua.Models;
+using Parcial1_Ap2_Lantigua.DAL;
+using System.Linq.Expressions;
+using Microsoft.VisualBasic;
+
+namespace Parcial1_Ap2_Lantigua.BLL
+{
+    public class ArticulosBLL
+    {
+        public static bool Guardar(Articulos articulo)
+        {
+            if (!Existe(articulo.ArticuloId))
+                return Insertar(articulo);
+            else
+                return Modificar(articulo);
+        }
+
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Articulos.Any(a => a.ArticuloId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        private static bool Insertar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Articulos.Add(articulo);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Articulos articulo)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(articulo).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var articulo = contexto.Articulos.Find(id);
+
+                if (articulo != null)
+                {
+                    contexto.Articulos.Remove(articulo);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Articulos Buscar(int id)
+        {
+            Articulos articulo = new Articulos();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                articulo = contexto.Articulos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return articulo;
+        }
+
+        public static List<Articulos> GetList(Expression<Func<Articulos, bool>> criterio)
+        {
+            List<Articulos> lista = new List<Articulos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Articulos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
